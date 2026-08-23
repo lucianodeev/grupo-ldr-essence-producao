57309945f99552c2e1b299bdd8f0e1df8df1e1f5
ldr-rh-campanhas <llucianouam@gmail.com>
2026-08-23 13:00:49 +0200

    Corrigir deploy SSR Netlify

--- apps/painel-ldr/vite.config.ts
+++ apps/painel-ldr/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     netlify(),
+    viteReact(),
   ],
 });
