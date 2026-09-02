c6fd2e5a5b68c5b377ee44ecb704577b72199573
ldr-rh-campanhas <llucianouam@gmail.com>
2026-09-02 09:24:09 -0700

    feat treinamento: ativar conteúdo protegido completo

--- apps/painel-ldr/src/content/do-mamao-ao-negocio.server.ts
+++ apps/painel-ldr/src/content/do-mamao-ao-negocio.server.ts
@@ -1,1 +1,12 @@
-PLACEHOLDER
+import { gunzipSync } from "node:zlib";
+import c1 from "./do-mamao-ao-negocio.chunk1";
+import c2 from "./do-mamao-ao-negocio.chunk2";
+import c3 from "./do-mamao-ao-negocio.chunk3";
+import c4 from "./do-mamao-ao-negocio.chunk4";
+import c5 from "./do-mamao-ao-negocio.chunk5";
+
+const TRAINING_GZIP_BASE64 = c1 + c2 + c3 + c4 + c5;
+
+export function getDoMamaoAoNegocioHtml() {
+  return gunzipSync(Buffer.from(TRAINING_GZIP_BASE64, "base64")).toString("utf8");
+}
