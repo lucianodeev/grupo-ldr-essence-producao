af2b89450839454e3cba2bd7a697e36eedc5970c
ldr-rh-campanhas <llucianouam@gmail.com>
2026-09-04 15:55:09 -0700

    fix pages: renovar carregamento do treinamento

--- training-card-v1.js
+++ training-card-v1.js
@@ -11,6 +11,5 @@
   }
 
   loadOnce("ldr-training-card-original-loader", "./training-card-v1-original-20260904.js?v=37394935");
-  loadOnce("ldr-platform-extra-cards-final-loader", "./platform-product-cards-final.js?v=20260904-filmux2");
-  loadOnce("ldr-training-details-i18n-loader", "./training-details-i18n-v1.js?v=20260905-i18n2");
+  loadOnce("ldr-platform-extra-cards-final-loader", "./platform-product-cards-final.js?v=20260905-livefix3");
 })();
